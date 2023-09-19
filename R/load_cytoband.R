ae00cdaa764dee5b8cfcf8809b9a81925f645a2f
Tom Lesluyes <t.lesluyes@gmail.com>
2023-09-19 16:36:22 +0200

    Remove comments

--- R/load_cytoband.R
+++ R/load_cytoband.R
@@ -9,11 +9,9 @@
 load_cytoband=function(assembly) {
   if (assembly=="hg19") {
     message("Loading hg19 data")
-    #utils::data("cytoband_hg19", package="myFun")
     load(system.file("extdata", "cytoband_hg19.rda", package="myFun"), envir=.GlobalEnv)
   } else if (assembly=="hg38") {
     message("Loading hg38 data")
-    #utils::data("cytoband_hg38", package="myFun")
     load(system.file("extdata", "cytoband_hg38.rda", package="myFun"), envir=.GlobalEnv)
   } else {
     stop("Unsupported assembly")
