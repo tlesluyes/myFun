0f3fcda2da9d17ad7643d33112cea60d65043e60
Tom Lesluyes <t.lesluyes@gmail.com>
2026-02-17 13:54:13 +0100

    occurrenceGRanges - Now compute percentages

--- R/occurrenceGRanges.R
+++ R/occurrenceGRanges.R
@@ -3,6 +3,7 @@
 #' @details This function gets the occurrence of events in a list of GRanges objects. All objects must have the same metadata columns and metadata must be TRUE/FALSE.
 #' @param myGRList a list of GRanges objects, each object should correspond to one CNA profile
 #' @param myMetadata a vector of metadata to consider
+#' @param computeP whether to compute the percentage of samples with specific metadata for each region (default: TRUE)
 #' @return A GRanges object with nSamples as the total number of samples and metadata columns with the occurrence of events
 #' @examples
 #' GR1 <- GenomicRanges::GRanges(seqnames="1",
@@ -14,14 +15,20 @@
 #' occurrenceGRanges(list(GR1, GR2), c("Gain", "Loss"))
 #' @author tlesluyes
 #' @export
-occurrenceGRanges <- function(myGRList, myMetadata) {
+occurrenceGRanges <- function(myGRList, myMetadata, computeP=TRUE) {
   checkGRlist(myGRList)
+  stopifnot(typeof(myMetadata)=="character")
+  stopifnot(length(computeP) == 1 && is.logical(computeP))
   stopifnot(all(sapply(myGRList, function(x) all(myMetadata %in% names(mcols(x)))))) # Make sure metadata exists
   OUT <- disjoin(Reduce(c, myGRList)) # Create a list of all regions
   OUT$nSamples <- countOverlaps(OUT, GRangesList(myGRList)) # Get the number of samples for each region
   for (i in myMetadata) { # For each metadata
     stopifnot(all(sapply(myGRList, function(x) all(mcols(x)[, i] %in% c(TRUE, FALSE))))) # Make sure metadata is TRUE/FALSE
     mcols(OUT)[, i] <- countOverlaps(OUT, GRangesList(lapply(myGRList, function(x) x[mcols(x)[, i], ]))) # Get the number of samples with specific metadata for each region
+    if (isTRUE(computeP)) {
+      stopifnot(! paste0(i, "_p") %in% names(mcols(OUT))) # Make sure percentage column does not already exist
+      mcols(OUT)[, paste0(i, "_p")] <- round(mcols(OUT)[, i] / OUT$nSamples * 100, 4) # Compute percentage
+    }
   }; rm(i)
   return(OUT)
 }
